945eb77caa3045342e77e91b58ff6fa14b1331e8
Kester-Broatch <59141647+Kester-Broatch@users.noreply.github.com>
2020-02-13 10:23:58 +0000

    GNC update
    Merge of GNC, CA and WP following

--- GNC.py
+++ GNC.py
@@ -4,8 +4,8 @@
   ' Add all important details regarding the program here
   '
   ' Project             : BAE Swarm Challenge - Group Design Project
-  ' Program name        : SAMPLE_CODE_gdpa.py
-  ' Author              : Romain Delabeye
+  ' Program name        : GNC.py
+  ' Author              : Romain Delabeye & Kester Broatch
   ' Adm. structure      : Cranfield University
   ' Date (YYYYMMDD)     : 20200205
   ' Purpose             : provide code structure
@@ -15,19 +15,15 @@
   '
   ' Date YYYYMMDD |  Author          | Ref       |Revision comment
   '-------------------------------------------------------------------
-  ' 20200205      |  Romain DELABEYE |           | Developing & Structuring - howto publish&subscribe; struct
+  ' 20200205      |  Romain DELABEYE |           | Developing & Structuring
   ' 20200206      |  Kester Broatch  |           | Added GNC 
-  '               |                  |           | 
+  ' 20200212      |  Romain DELABEYE |           | Merge CA with wp guidance, state, TM, etc.
   '               |                  |           | 
 
 
 TODO:
-""" Put here all your tasks """
-(You can also add TODO anywhere in the code if you need to come back to a certain line later on)
 
--
--
--
+
 
 ''-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------'''
 '''
@@ -36,27 +32,107 @@ TODO:
 '------------------------------------------------------------------------------------------------------------------------
 '''
 
-## import libraries - comment the undesired libraries
+## System
 import os
 import time
+from uptime import uptime
 
+
+# Math
 import math
 import numpy as np
 from numpy import linalg as npla
+from geomdl import NURBS
+
+# ROS & piloting
 import rospy
+from sensor_msgs.msg import *
 from std_msgs.msg import *
 import message_filters
+from dronekit import *
+try : 
+    from test.msg import Action, Agent, position_detection, multipositions, testimage
+    from CA_functions import getDistanceAngle
+except Exception as e:
+    print("!Could not import some stuff\n",e)
+
+# Perso
+import config as cf
+from CA_functions import getDistanceAngle
+
 
 '''
 '------------------------------------------------------------------------------------------------------------------------
 '    Toolbox
 '------------------------------------------------------------------------------------------------------------------------
 '''
-from dronekit import *
-from sensor_msgs.msg import NavSatFix, Imu
-from test.msg import position, multipositions
-from CA_functions import getDistanceAngle
-from uptime import uptime
+
+def NED2WF(north_east_down, pos_offset_Float32MultiArray = cf.POS_OFFSET):
+    offset_to_origin = np.array(pos_offset_Float32MultiArray.data)
+    current = np.array(north_east_down)
+    newloc = Float32MultiArray()
+    newloc.data = np.array(offset_to_origin-current).tolist()
+    return newloc
+
+def WF2NED(north_east_down, pos_offset_Float32MultiArray = cf.POS_OFFSET):
+    offset_to_origin = np.array(pos_offset_Float32MultiArray.data)
+    current = np.array(north_east_down)
+    newloc = Float32MultiArray()
+    newloc.data = np.array( offset_to_origin + current).tolist()
+    return newloc
+    
+def spline(wi, wi1, wi2, deg = 2, delta = .05):
+    # w_previous = np.array(w_previous)
+    # wi = np.array(wi)
+    # w_next = np.array(w_next)
+    curve = NURBS.Curve()
+    curve.degree = deg
+    curve.ctrlpts = [wi, wi1, wi2]
+    curve.knotvector = [0, 0, 0, 1, 1, 1]
+    curve.delta = delta
+    return curve.evalpts
+
+def calc_heading_speed(Wi, Wnxt, pos, current_heading, current_velocity, K_carrot = 1., Delta_carrot = .1):
+    '''
+    provide the new heading & velocity:
+    INPUT:
+    - Wi , Wnxt , pos : horizontal waypoint positions & current location
+    - current_heading , current_velocity : heading & vel
+    - K_carrot : velocity tuning
+    - Delta_carrot : heading tuning
+    OUTPUT:
+    - new_heading , new_velocity
+    '''
+    if len(current_velocity) >1: current_velocity = npla.norm(np.array(current_velocity[:2])) # take the norm of the velocity if given as a vector
+    Ru = npla.norm(np.array(Wi[:2])-np.array(pos[:2]))
+    theta = np.arctan2(Wnxt[1] - Wi[1], Wnxt[0] - Wi[0])
+    thetau = np.arctan2(pos[1] - Wi[1], pos[0] - Wi[0])
+    delta_theta = theta-thetau
+    R = np.sqrt(Ru*Ru - (Ru*np.sin(delta_theta))*(Ru*np.sin(delta_theta)))
+    xt , yt = (R+Delta_carrot)*np.cos(theta) , (R+Delta_carrot)*np.sin(theta)
+    new_heading = np.arctan2(yt-pos[1], xt-pos[0])
+    new_velocity = min(K_carrot*(new_heading-current_heading)*current_velocity, cf.MAX_VELOCITY)
+    return new_heading, new_velocity
+
+def has_reached_waypoint2D(wi, pos, radius = .5):
+    '''
+    True if waypoint reached, i.e. if distance of pos from wi is less than radius metres.
+    2D ONLY (horizontal distance with projection)
+    '''
+    return npla.norm(np.array(wi[:2])-np.array(pos[:2])) < radius
+def has_reached_waypoint3D(wi, pos, radius = .5):
+    '''
+    True if waypoint reached, i.e. if distance of pos from wi is less than radius metres.
+    3D distance
+    '''
+    return npla.norm(np.array(wi)-np.array(pos)) < radius
+
+
+
+    
+
+
+
 
 '''
 '------------------------------------------------------------------------------------------------------------------------
@@ -64,40 +140,120 @@ from uptime import uptime
 '------------------------------------------------------------------------------------------------------------------------
 '''
 
+def callback_TaskManager(data): #TODO subcribe to TC, SA, MC
+    '''
+    Task Manager: 
+    - enable/disable functionalities with respect to miscellaneous cases.
 
-def callback_VehicleState(data):
-    global vehicle
-
-    # Imports vehicle state from pixhawk and publishes vehicle states to ros
-    try:
-
-        #TODO get xyz, gps and imu from vehicle object
-
-        # Publishing GPS
-        gps = NavSatFix()
-        gps_pub = rospy.Publisher('VehicleState/gps', NavSatFix, queue_size=10)
-        gps_rate = rospy.Rate(10) # setup publishing frequency, in Hz
-        #rospy.loginfo(gps)  # [optional] print & save data in the node's terminal
-        gps_pub.publish(gps)
-        gps_rate.sleep() # to reach rospy.Rate(...) frequency.
-
-        # Publishing Imu
-        imu = Imu()
-        imu_pub = rospy.Publisher('VehicleState/imu', Imu, queue_size=10)
-        imu_rate = rospy.Rate(10) # setup publishing frequency, in Hz
-        #rospy.loginfo(imu)  # [optional] print & save data in the node's terminal
-        imu_pub.publish(imu)
-        imu_rate.sleep() # to reach rospy.Rate(...) frequency.    
-
-        pass
-    except rospy.ROSInterruptException:
-        pass
-    
-def callback_GNCTaskManager(data): #TODO subcribe to TC, SA, MC
-    global vehicle
+    note: collision avoidance triggers itself, where the other functionalities wait to receive clearance to operate.
+    '''
+    global vehicle, agent
 
     # Decides which GNC algorithm should be used, based upon TC, SA, MC
     try:
+        if not agent.enable_agent:
+            # Disable all functionalities at the same time (first level of safety)
+            agent.clear_pause = False
+            agent.clear_ca = False
+            agent.clear_wp = False
+            agent.release = False
+        else:
+            if agent.clear_pause:
+                # Disable all functionalities except pause when pause ordered
+                agent.clear_ca = False
+                agent.clear_wp = False
+                agent.release = False
+            else:
+                if agent.clear_ca :
+                    # Disable functionalities when collision avoidance is triggered
+                    agent.clear_wp = False
+                    agent.release = False                
+                else:
+                    agent.clear_wp = True
+                    agent.release = True
+        
+
+            pass
+    except rospy.ROSInterruptException:
+        pass
+
+
+def callback_wp(data):
+    '''
+    Waypoint following
+    TODO: make sure command is meant for this agent
+    #TODO : extend "==" to "almost equal", same for "!=" : heading & vel
+    # TODO: velocity returned as a vector (dir = heading; norm = as defined) in carrot chasing above
+    '''
+    global vehicle, agent
+    try:
+        if agent.clear_wp and agent.action.type == "wp":
+            # check if next waypoint reached & update waypoints
+            iWP = agent.action.iWP
+
+            # start with the appropriate altitude
+            if iWP == 0:
+                vehicle.simple_goto() # TODO use the wp loc in the drone's frame (known as NED, not WF)
+
+            # Carrot chasing
+            new_heading , new_groundspeed = calc_heading_speed(agent.action.wp[iWP-1],agent.action.wp[iWP],agent.pos, agent.heading, agent.groundspeed)
+            # Update heading & vel in this order
+            if vehicle.heading != new_heading:
+                vehicle.heading = new_heading
+            else:
+                vehicle.groundspeed = new_groundspeed
+
+            if iWP < len(agent.action.wp)-1 and has_reached_waypoint2D(agent.action.wp[iWP], agent.pos):
+                agent.action.iWP += 1
+                
+
+            # debug
+            if cf.debug_GNC_wp :
+                # rospy.loginfo(agent.ned_from_start)
+                pass
+
+            rate.sleep()
+
+            pass
+    except rospy.ROSInterruptException:
+        pass
+
+def callback_VehicleState(data):
+    '''
+    Import vehicle state from pixhawk and publishes vehicle states to ros
+    '''
+    global vehicle, agent
+
+    try:
+        # Agent ID
+        if agent.id != cf.AGENT_ID: agent.id = cf.AGENT_ID
+
+        # Update battery level
+        agent.battery = vehicle.battery.level
+
+        # Update location
+        agent.ned_from_start = vehicle.location.local_frame  # (north, east, down) from init loc
+        agent.pos = NED2WF(agent.ned_from_start)
+
+        # Update velocity
+        agent.velocity = vehicle.velocity
+        agent.groundspeed = npla.norm(np.array(agent.velocity[:2]))
+
+        # Update attitude
+        agent.ned_attitude = vehicle.attitude
+        agent.attitude = agent.ned_attitude
+        agent.heading = vehicle.heading
+
+        # Publish updated state
+        pub_agent_GNC2SA.publish(agent)
+
+        # debug
+        if cf.debug_GNC_VehicleState :
+            rospy.loginfo(agent.ned_from_start)
+            rospy.loginfo(agent.ned_attitude)
+            rospy.loginfo(agent.battery)
+
+        rate.sleep()
 
         pass
     except rospy.ROSInterruptException:
@@ -129,10 +285,10 @@ def callback_CollisionAvoidance(data):
             @vehicle.on_message('HWSTATUS') # if no message found it will not display but will not error
             def listener(self,name,message):
                 print(message)  
-
         pass
     except rospy.ROSInterruptException:
         pass
+
 
 def callback_Test(data): 
     global vehicle
@@ -169,68 +325,91 @@ def callback_Test(data):
     except rospy.ROSInterruptException:
         pass
 
-
 if __name__ == "__main__":
-
 
     ## ----------------------- SETUP --------------------- [Code to run only once]
     ## Setup node (name)
     rospy.init_node('GNC', anonymous=False)
+    rate = rospy.Rate(10)
 
-    # Connect to vehicle
-    # vehicle = connect('/dev/ttyTHS1', wait_ready=True, baud=115200)
-    # ṢITL
-    vehicle = connect('tcp:127.0.0.1:5762', wait_ready=True) 
-
-    # Testing 
-    print('Connection successful')
-    vehicle.mode = VehicleMode("GUIDED")
-    while not vehicle.is_armable:
-        print(" Waiting for vehicle to initialise...")
-        time.sleep(1)
-    vehicle.armed = True
-    while not vehicle.armed:
-        print(" Waiting for arming...")
-        time.sleep(1)
-    time.sleep(1)
-    vehicle.simple_takeoff(15)
-    time.sleep(5)
-    vehicle.airspeed = 3
-    point1 = LocationGlobalRelative(-35.36137565, 149.16472783, 20)
-    vehicle.simple_goto(point1)
-
-    time.sleep(30)
-
-
-    # -35.36137565 149.16472783
-
-    # TODO - load a config file of all parameters
-
-    # do nothing
+    # [do nothing] add headers to some classes
     quickFix_header = Header()
     quickFix_header.stamp = rospy.Time.now()
     String.header = quickFix_header # adds a header in String to synchronize subscribing
     Float64.header = quickFix_header
+    Agent.header = quickFix_header
+
+    # Connect to vehicle (choose host (SITL/laptop/jetson...) in config file)
+    vehicle = connect(cf.Pixhawk_USBpath, wait_ready=True, baud=115200)
+    print('Connection successful')
+
+    # Setup mode
+    vehicle.mode = VehicleMode("GUIDED")
+    while not vehicle.is_armable:
+        print(" Waiting for vehicle to initialise...")
+        time.sleep(.5)
+
+    # Arm vehicle
+    vehicle.armed = True
+    while not vehicle.armed:
+        print(" Waiting for arming...")
+        time.sleep(.5)
+    time.sleep(1)
+
+    if True:
+        # simple test sequence
+        time.sleep(1)
+        vehicle.simple_takeoff(15)
+        time.sleep(5)
+        vehicle.airspeed = 3
+        point1 = LocationGlobalRelative(-35.36137565, 149.16472783, 20)
+        vehicle.simple_goto(point1)
+        time.sleep(30)
+
+
+    # Collision avoidance
+    request_clear_ca = False
+
+
+    # Vehicle State setup
+    agent = Agent()
+    agent.pos_offset = Float32MultiArray(data = cf.POS_OFFSET)
+
+    # wp following
+    iWP = 1
+
+    
 
     ## ----------------------- PUBLISHERS --------------------- [Code to run only once]
-
+    # pub agent from GNC to SA
+    pub_agent_GNC2SA = rospy.Publisher('agent_GNC2SA', Agent, queue_size=10)
 
 
 
     ## ----------------------- SUBSCRIBERS/CALLBACKS --------------------- [Code to run only once]
 
-    # VEHICLE STATE
-    ###############
-    # ts_VehicleState = message_filters.TimeSynchronizer(message_filters.Subscriber('clock/time', Float64),10)
-    # ts_VehicleState.registerCallback(callback_VehicleState)
-    rospy.Subscriber("time",Float64,callback_VehicleState)
     
     # GNC TASK MANAGER
     ##################
     # ts_GNCTaskManager = message_filters.TimeSynchronizer([message_filters.Subscriber('SA', String),
     #                                        message_filters.Subscriber('TC', String)], 10)
     # ts_GNCTaskManager.registerCallback(callback_GNCTaskManager)
-    rospy.Subscriber("time",Float64,callback_GNCTaskManager)
+    rospy.Subscriber("time",Float64,callback_TaskManager)
+
+
+    # VEHICLE STATE
+    ###############
+    # ts_VehicleState = message_filters.TimeSynchronizer(message_filters.Subscriber('clock/time', Float64),10)
+    # ts_VehicleState.registerCallback(callback_VehicleState)
+    rospy.Subscriber("time",Float64,callback_VehicleState)
+
+
+    # WAYPOINT FOLLOWING
+    ###############
+    # ts_VehicleState = message_filters.TimeSynchronizer(message_filters.Subscriber('clock/time', Float64),10)
+    # ts_VehicleState.registerCallback(callback_VehicleState)
+    rospy.Subscriber("time",Float64,callback_wp)
+
 
     # COLISION AVOIDANCE
     #####################
@@ -238,7 +417,13 @@ if __name__ == "__main__":
     # ts_CollisionAvoidance.registerCallback(callback_CollisionAvoidance)
     rospy.Subscriber("bbox_position2", multipositions, callback_CollisionAvoidance)
 
+
+    # TEST
+    #####################
     rospy.Subscriber("time",Float64, callback_Test)
+
+
+
 
     # Prevent undesired program ending
     rospy.spin()
